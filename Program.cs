9f6ce0525180776a14d3f423d60a15f175c55359
L Graham <lgraham92@gmail.com>
2020-08-11 09:53:10 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name; // Declare a variable named string
+            name = "James";
+
+
+            Console.WriteLine(name);
+        }
+    }
+}
