2c89b9799c2dc5731cf49d3416cd42b0ad8ea257
L Graham <lgraham92@gmail.com>
2020-08-11 10:29:11 -0600

    Added implicit and explicit integer-string conversions;

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
+            // Declare a variable using the datatype followed by the name
+            string text; // Strings store text
+            char character; // Stores a single character (Including space, newline, etc)
+            int wholeNumber; // Int stores a number without a decimal point (positive or negative)
+            // byte, short, and long are other int types with varying size limits
+            double deciNumber; // Double stores a number with a decimal point
+            // float and decimal are alternatives with quirks. Google them if you plan to use them
+            bool boolean; // Bool stores a true or false value.
 
-            string name; // Declare a variable named string
-
+            string name; // Declare our variable
+            int age;
 
             Console.WriteLine("Please Enter Your Name:"); // Tell the user what we're expecting them to input
             name = Console.ReadLine(); // Assign a value to the variable that we get from the user
          
-            Console.WriteLine("Thank you, " + name);
+            Console.WriteLine("Please Enter Your Age:"); // Tell the user what we're expecting them to input
+            age = int.Parse(Console.ReadLine()); // Convert the user's input to an integer and store it.
+            // If the user enters something that isn't an integer, it will error.
+
+            Console.WriteLine("Thank you, " + name + ". You are " + age + " years old."); // Retrieve the value from the variable and send it to the output.
 
 
 
